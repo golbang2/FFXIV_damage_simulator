ccfc2b4cc50812ee873cfb5c0af5b48c2b9f1053
golbang2 <atgolbang2@gmail.com>
2022-08-13 03:44:15 +0900

    Update simulate_from_log.py

--- simulate_from_log.py
+++ simulate_from_log.py
@@ -7,3 +7,7 @@ Created on Fri Aug 12 14:58:37 2022
 
 import ffxiv_calculate_damage as f
 import numpy as np
+import read_log
+
+mach = read_log.read_from_csv("D:/game_plan/FFXIV/Jomusse_first.csv")
+

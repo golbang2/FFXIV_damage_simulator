2ffc1c1ac0dd1b32850b0544a4aa5af6e79fe023
golbang2 <atgolbang2@gmail.com>
2022-08-22 16:32:18 +0900

    add repeat simulate

--- simulate_from_log.py
+++ simulate_from_log.py
@@ -13,14 +13,56 @@ import bard
 import pandas as pd
 
 class read_machinist():
-    def __init__(self,file_name="D:/game_plan/FFXIV/Jomusse_first.csv"):
-        active_log = read_log.read_from_csv(file_name).activation_log
-        self.character = mach.machinist(2.5, 2121, 1626,1615,2575,120,400)
+    def __init__(self,file_name="D:/game_plan/FFXIV/FF Logs - Combat Analysis for FF1.csv"):
         
-        self.simulated_damage = self.simulate_from_log(active_log)
+        reading_log = read_log.read_from_csv(file_name)
+        self.active_log = reading_log.add_potency()
+        self.period = np.sum(self.active_log['global_cooldown']) - 2.5
+        self.character = mach.machinist(gc = 2.5, cr = 2121, dh = 1626, dt = 1615, stat = 2575, wd = 120,spd = 400, period = self.period)
         
-    def simulate_from_log(self,active_log):
+    def simulate_from_log(self):
         damage_list = []
+        crit_list = []
+        dhit_list = []
+        
+        for i in self.active_log.index.tolist():
+            if self.active_log['auto_attack'][i]:
+                dmg,is_cr,is_dh = self.character.auto_shot()
+                crit_list.append(is_cr)
+                dhit_list.append(is_dh)
+                damage_list.append(dmg)
+            elif self.active_log['reassemble'][i]:
+                dmg,is_cr,is_dh = self.character.calculate_dmg(self.active_log['potency'][i],reassemble=1)
+                crit_list.append(is_cr)
+                dhit_list.append(is_dh)
+                damage_list.append(dmg)
+            else:
+                dmg,is_cr,is_dh = self.character.calculate_dmg(self.active_log['potency'][i],reassemble=0)
+                crit_list.append(is_cr)
+                dhit_list.append(is_dh)
+                damage_list.append(dmg)
+        simulated_damage = damage_dataframe(damage_list, crit_list, dhit_list)
+        return simulated_damage
+    
+    def repeat_simulate(self,epoch):
+        log_list = []
+        for i in range(epoch):
+            log_list.append(self.simulate_from_log())
+            
+        dps_list = []
+        for i in log_list:
+            dps = i['damage']/self.period
+            dps_list.append(dps)
+        return log_list
+            
+class read_bard():
+    def __init__(self,file_name):
+        active_log = read_log.read_from_csv(file_name).activation_log
+        self.character = bard.bard(gc = 2.08, cr = 2229, dh = 1662, dt = 1381, stat = 2575, wd = 120,spd = 479, period = 120)
+        self.simulated_damage = self.simulate_from_log(active_log)
+    
+    def simulate_from_log(self,active_log):
+        damage_list=[]
         crit_list = []
         dhit_list = []
         
@@ -30,30 +72,14 @@ class read_machinist():
                 crit_list.append(is_cr)
                 dhit_list.append(is_dh)
                 damage_list.append(dmg)
-            elif active_log['reassemble'][i]:
-                dmg,is_cr,is_dh = self.character.calculate_dmg(active_log['potency'][i],reassemble=1)
-                crit_list.append(is_cr)
-                dhit_list.append(is_dh)
-                damage_list.append(dmg)
-            else:
-                dmg,is_cr,is_dh = self.character.calculate_dmg(active_log['potency'][i],reassemble=0)
-                crit_list.append(is_cr)
-                dhit_list.append(is_dh)
-                damage_list.append(dmg)
-        simulated_damage = damage_dataframe(damage_list, crit_list, dhit_list)
-        return simulated_damage
-        
-class read_bard():
-    def __init__(self,file_name):
-        active_log = read_log.read_from_csv(file_name).activation_log
-        self.character = bard.bard(2.5, 2121, 1626,1615,2575,120,400)
-        
-        self.simulated_damage = self.simulate_from_log(active_log)
-        
-        
-        
+            
 def damage_dataframe(dmg_list, crit_list, dhit_list):
     df_damage = pd.DataFrame(dmg_list, columns = ['damage'])
     df_damage.insert(1,'cr',crit_list)
     df_damage.insert(2,'dh',dhit_list)
     return df_damage
+
+
+if __name__=='__main__':
+    read_mach = read_machinist()
+    data = read_mach.simulate_from_log()

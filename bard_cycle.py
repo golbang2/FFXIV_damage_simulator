48f57ab53a27be6faf1d0012811f53869cb1d6fc
golbang2 <32033086+golbang2@users.noreply.github.com>
2022-09-12 16:36:50 +0900

    bard dealcycle

---
+++ bard_cycle.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 16:26:50 2022
+
+@author: Taeyoon
+"""
+
+import bard
+
+def opening(bard_class):
+    bard_class.stormbite()
+    bard_class.wanderer_minuet()
+    bard_class.raging()
+    bard_class.causticbite()
+    bard_class.empyreal()
+    bard_class.blood()
+    bard_class.burst_shot()
+    bard_class.radient()
+    bard_class.battle_voice()

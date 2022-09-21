0d7fff2eec447db0a46cb04871ddf12e77a22349
golbang2 <32033086+golbang2@users.noreply.github.com>
2022-09-21 22:01:26 +0900

    Update bard_cycle.py

--- bard_cycle.py
+++ bard_cycle.py
@@ -7,82 +7,95 @@ Created on Mon Sep 12 16:26:50 2022
 
 import bard as job
 
-def opening(bard):
-    bard.stormbite()
-    bard.wanderer_minuet()
-    bard.raging()
-    bard.causticbite()
-    bard.empyreal()
-    bard.blood()
-    bard.burst_shot()
-    bard.radient()
-    bard.battle()
-    bard.burst_shot()
-    if bard.straight:
-        bard.sidewinder()
-        bard.burst_shot()
-        bard.barrage()
-        bard.burst_shot()
-        bard.burst_shot()
-        bard.empyreal()
-        bard.iron_jaws()
+def opening(agent):
+    agent.stormbite()
+    agent.wanderer_minuet()
+    agent.raging()
+    agent.causticbite()
+    agent.empyreal()
+    agent.blood()
+    agent.burst_shot()
+    agent.radient()
+    agent.battle()
+    agent.burst_shot()
+    if agent.straight:
+        agent.sidewinder()
+        agent.burst_shot()
+        agent.barrage()
+        agent.burst_shot()
+        agent.burst_shot()
+        agent.empyreal()
+        agent.iron_jaws()
     else:
-        bard.barrage()
-        bard.burst_show()
-        bard.sidewinder()
-        bard.burst_show()
-        bard.burst_show()
-        bard.burst_show()
-        bard.empyreal()
-        bard.iron_jaws()
+        agent.barrage()
+        agent.burst_show()
+        agent.sidewinder()
+        agent.burst_show()
+        agent.burst_show()
+        agent.burst_show()
+        agent.empyreal()
+        agent.iron_jaws()
 
-def burst(bard):
-    gc_in_minuet(bard)
-    bard.wanderer_minuet()
-    bard.raging()
-    gc_in_minuet(bard)
-    ngc_in_minuet(bard)
-    ngc_in_minuet(bard)
-    gc_in_minuet(bard)
-    bard.radient()
-    bard.battle()
-    gc_in_minuet(bard)
+def burst(agent):
+    gc_in_minuet(agent)
+    agent.wanderer_minuet()
+    agent.raging()
+    gc_in_minuet(agent)
+    ngc_in_minuet(agent)
+    ngc_in_minuet(agent)
+    gc_in_minuet(agent)
+    agent.radient()
+    agent.battle()
+    gc_in_minuet(agent)
 
-def gc_in_minuet(bard):
-    if (bard.dot_caustic<3 or bard.dot_storm<3):
-        bard.iron_jaws()
-    elif bard.available_blast:
-        bard.blast_arrow()
-    elif (bard.buff_raging >0 and bard.buff_radient>0 and bard.buff_battle>0 and bard.soul>80):
-        bard.apex_arrow()
+def gc_in_minuet(agent):
+    if (agent.dot_caustic<3 or agent.dot_storm<3):
+        agent.iron_jaws()
+    elif agent.available_blast:
+        agent.blast_arrow()
+    elif (agent.buff_raging >0 and agent.buff_radient>0 and agent.buff_battle>0 and agent.soul>80):
+        agent.apex_arrow()
+    else:
+        agent.burst_shot()
+        
 
-def ngc_in_minuet(bard):
-    if bard.stack_wanderer==3:
-        bard.pitch()
-    elif bard.cool_sidewinder<0:
-        bard.sidewinder()
-    elif bard.cool_empyreal<0:
-        bard.empyreal()
-    elif bard.available_blood>0:
-        bard.blood()
+def ngc_in_minuet(agent):
+    if agent.stack_wanderer==3:
+        agent.pitch()
+    elif agent.cool_sidewinder<0:
+        agent.sidewinder()
+    elif agent.cool_empyreal<0:
+        agent.empyreal()
+    elif agent.available_blood>0:
+        agent.blood()
     
-    if bard.wanderer<3:
-        bard.mage_ballad()
+    if agent.wanderer<3:
+        agent.mage_ballad()
         
-def gc_in_mage(bard):
-    if (bard.dot_caustic<3 or bard.dot_storm<3):
-        bard.iron_jaws()
-    elif bard.available_blast:
-        bard.blast_arrow()
-    elif (bard.buff_raging >0 and bard.buff_radient>0 and bard.buff_battle>0 and bard.soul>80):
-        bard.apex_arrow()
+def gc_in_mage(agent):
+    if (agent.dot_caustic<3 or agent.dot_storm<3):
+        agent.iron_jaws()
+    elif agent.soul>80:
+        agent.apex_arrow()
+    elif agent.available_blast:
+        agent.blast_arrow()
+    elif (agent.buff_raging >0 and agent.buff_radient>0 and agent.buff_battle>0 and agent.soul>80):
+        agent.apex_arrow()
+    else:
+        agent.burst_shot()
         
-def ngc_in_mage(bard):
-    if bard.stack_wanderer==3:
-        bard.pitch()
-    elif bard.cool_sidewinder<0:
-        bard.sidewinder()
-    elif bard.cool_empyreal<0:
-        bard.empyreal()
-    elif bard.available_blood>0:
-        bard.blood()
+def ngc_in_mage(agent):
+    if agent.stack_wanderer==3:
+        agent.pitch()
+    elif agent.cool_sidewinder<0:
+        agent.sidewinder()
+    elif agent.cool_empyreal<0:
+        agent.empyreal()
+    elif agent.available_blood>0:
+        agent.blood()
+
+def gc_in_army(agent):
+    if (agent.dot_caustic<3 or agent.dot_storm<3):
+        agent.iron_jaws()
+    else:
+        agent.burst_shot()
